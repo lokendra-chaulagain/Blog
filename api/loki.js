fbcc5a2c4ce298aa77347fa8297e12e3b898a39b
lokendra <lokendrachaulagain803@gmail.com>
2022-04-03 14:23:17 +0545

    init

---
+++ api/loki.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function loki(props) {
+  return (
+    <div>loki demo file</div>
+  )
+}
+
+loki.propTypes = {}
+
+export default loki
