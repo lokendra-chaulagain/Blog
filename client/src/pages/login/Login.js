5441cb7131ccadaa956117d8c967f35fd7e5ec3c
lokendra <lokendrachaulagain803@gmail.com>
2022-04-04 17:20:30 +0545

    loginPage

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -6,13 +6,13 @@ function Login() {
         <div className='login'>
             <div className="loginWrapper">
                 <div className="formContainer">
-                    <label>Email</label>
-                    <input type="email" placeholder='Email' />
-                    <label>Password</label>
-                    <input type="password" placeholder='Password' />
-                    <button>Login</button>
-                    <span>Dont have an account</span>
-                    <button>Create an Account</button>
+                    <label className='labelText'>Email</label>
+                    <input className='loginInput' type="email" placeholder='Email' />
+                    <label className='labelText'>Password</label>
+                    <input className='loginInput' type="password" placeholder='Password' />
+                    <button className='loginButton'>Login</button>
+                    <span className='dontHaveAccountTxt'>Dont have an account ?</span>
+                    <button className='loginButton'>Create an Account</button>
 
 
                 </div>
