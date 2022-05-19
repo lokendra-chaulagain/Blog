28e500eaa447b7c93f0de848d027a7a111e3e1c2
lokendra <lokendrachaulagain803@gmail.com>
2022-05-19 14:39:30 +0545

    write page responsive

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,35 +1,3 @@
-// import React from 'react'
-// import "./post.scss"
-// import { Link } from 'react-router-dom'
-
-// function Post({ post }) {  //taking post props from Posts.js
-//     return (
-//         <div className='post'>
-//             <img className='postImg' src={post.postPic } alt="" />
-
-//             <Link to={`/post/${post._id}`} className="link" >   {/* <Route path="/post/:postId" element={<PostRead />} /> */}
-//                 <span className="postTitle">{post.title}</span>
-//             </Link >
-
-//             <div className="postCats">
-//                 {post.categories.map((c,key) => (
-//                     <span key={key}>{c.name}</span>
-//                 ))}
-//             </div>
-
-//             <div className='timeLocationRow'>
-//                 <span className='postTime'>{new Date(post.createdAt).toDateString()}</span>
-//                 <i className="locationIcon fa-solid fa-location-dot"></i>
-//                 <span className="locationText">Kathmandu Nepal</span>
-//                 <p className='postDescription'>{post.desc}</p>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Post
-
-//============================================================>
 import React from "react";
 import "./post.scss";
 import { Link } from "react-router-dom";
