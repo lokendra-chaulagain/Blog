90237e29bd1f5c683747d606ec0c837ddcd9bae3
lokendra <lokendrachaulagain803@gmail.com>
2022-06-17 12:29:20 +0545

    deploy

--- server/index.js
+++ server/index.js
@@ -37,6 +37,6 @@ app.use((error, req, res, next) => {
 });
 
 //Port listening
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port " + process.env.PORT);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running");
 });
